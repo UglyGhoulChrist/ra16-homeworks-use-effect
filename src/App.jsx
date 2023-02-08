5eaeeb206e49621f2c175777c7d948d335da92e3
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-09 00:45:30 +0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const URL_USERS =
+    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json";
+
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    fetch(URL_USERS)
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+      .catch(() => {})
+      .finally(() => {});
+  }, []);
+
   return <div className="App">Use Effect</div>;
 }
 
