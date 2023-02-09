a5c8f6263c640fd19b6c664a87c9e5f882e27953
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-10 01:32:54 +0300

    v1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,44 @@ import Details from "./components/Details";
 import List from "./components/List";
 
 function App() {
-  const URL_USERS =
-    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json";
-
+  const URL_PATH =
+    "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/";
+  // Все Юзеры
   const [users, setUsers] = useState([]);
+  // Путь до Юзера по которому кликнули
+  const [pathUser, setPathUser] = useState(null);
+  // Детали Юзера по которому кликнули
   const [detailsUser, setDetailsUser] = useState({});
-  console.log(users);
+  // Загружаем ?
+  const [isLoading, setIsLoading] = useState(false);
+
+  function getUsers(url, fun) {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => fun(json))
+      .catch(() => {})
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 
   useEffect(() => {
-    fetch(URL_USERS)
-      .then((response) => response.json())
-      .then((json) => setUsers(json))
-      .catch(() => {})
-      .finally(() => {});
-  }, []);
+    // Загрузка всех Юзеров
+    !users.length && getUsers(URL_PATH + "users.json", setUsers);
+    // Загрузка Юзера по ИД
+    pathUser && getUsers(pathUser, setDetailsUser);
+  }, [pathUser]);
 
   const handleUser = (id) => {
-    fetch(
-      `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${id}.json`
-    )
-      .then((response) => response.json())
-      .then((json) => setDetailsUser(json))
-      .catch(() => {})
-      .finally(() => {});
+    setPathUser(URL_PATH + id + ".json");
   };
 
   return (
     <div className="App">
       <List handleUser={handleUser} users={users} />
-      <Details {...detailsUser} />
+      {pathUser &&
+        ((isLoading && "Loading ...") || <Details {...detailsUser} />)}
     </div>
   );
 }
